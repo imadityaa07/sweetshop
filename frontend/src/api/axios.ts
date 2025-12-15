2f7ba23f4d98bc8f9d273f898b87f36ab5da5819
Aditya Niphade <imadityaa07@gmail.com>
2025-12-16 03:17:33 +0530

    Point frontend to production backend API

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:10000/api',
+  baseURL: 'https://sweetshop-ptz6.onrender.com/api',
   headers: {
     'Content-Type': 'application/json'
   }
